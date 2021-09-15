0c1610f5e9a1cab400ac87fb70a8425bf0a3f394
brbvi <vito.borba@gmail.com>
2021-09-14 21:40:56 -0300

    feat: initial configs

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from './style'
+
+export const Header: React.FC = () => {
+  return (
+    <Container>
+      <img src="" alt="" />
+      <div>
+        <a href="">Home</a>
+        <a href="">Todos os produtos</a>
+        <a href="">Contato</a>
+      </div>
+    </Container>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,6 +8,17 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root{
+    --body: #F6FAFF;
+    --green-top: #85c226;
+    --green-bottom: #00923f;
+    --brown: #854b3f;
+  }
+
+  body{
+    background: var(---body);
+  }
+
   button, input {
     background: 0;
     border: 0;
